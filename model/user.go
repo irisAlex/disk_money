3f8ffd865697655dfbfa095a84d993e35c1b4b9e
李建维 <lijianwei@hualala.com>
2024-01-11 18:13:48 +0800

    add http proxy

--- model/user.go
+++ model/user.go
@@ -1,9 +1,17 @@
 package model
 
+import "time"
+
 type RegisterUserInfo struct {
-	User  string `json:"user" bson:"user" valid:"-"`
-	Pwd   string `json:"pwd" bson:"pwd" valid:"-"`
-	Email string `json:"email" bson:"email" valid:"-"`
+	User        string    `json:"user" bson:"user" valid:"-"`
+	Pwd         string    `json:"pwd" bson:"pwd" valid:"-"`
+	Email       string    `json:"email" bson:"email" valid:"-"`
+	RealIP      string    `json:"real_ip" bson:"real_ip"`
+	ExpiredTime time.Time `json:"expired_time" bson:"expired_time"`
+	CreateTime  time.Time `json:"create_time" bson:"create_time"`
+	DownTime    int       `json:"down_time" bson:"down_time"`
+	Vip         int       `json:"vip" bson:"vip"`
+	SetMeal     int       `json:"set_meal" bson:"set_meal"`
 }
 
 type VerifyUserInfo struct {
@@ -14,6 +22,18 @@ type VerifyUserInfo struct {
 type LoginUserInfo struct {
 	User string `json:"user"`
 	Pwd  string `json: "pwd"`
+}
+
+type AccountInfo struct {
+	User        string    `json:"user" bson:"user" valid:"-"`
+	Pwd         string    `json:"pwd" bson:"pwd" valid:"-"`
+	Email       string    `json:"email" bson:"email" valid:"-"`
+	RealIP      string    `json:"real_ip" bson:"real_ip"`
+	ExpiredTime time.Time `json:"expired_time" bson:"expired_time"`
+	CreateTime  time.Time `json:"create_time" bson:"create_time"`
+	DownTime    int       `json:"down_time" bson:"down_time"`
+	Vip         int       `json:"vip" bson:"vip"`
+	SetMeal     int       `json:"set_meal" bson:"set_meal"`
 }
 
 func TableName() string {
